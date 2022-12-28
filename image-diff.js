8f35bfef5e92262c5ba63fea6b7637e91c2c5cce
andylinpersonal <andylinpersonal@gmail.com>
2022-12-28 17:41:07 +0800

    Add pane box

--- image-diff.js
+++ image-diff.js
@@ -1,6 +1,145 @@
 (function () {
 	'use strict';
 
+	/**
+	 * @description https://dev.to/ndesmic/how-to-make-a-pan-and-zoom-control-with-web-components-4ji6
+	*/
+	class ImageDiffPanBox extends Polymer.Element {
+		static get is() {
+			return 'image-diff-pan-box';
+		}
+
+		static get template() {
+			return Polymer.html`
+			<style>
+				:host {
+					display: block;
+				}
+				#viewport { height: 100%; width: 100%; overflow: auto; cursor: grab; }
+				#viewport.manipulating { cursor: grabbing; }
+			</style>
+			<div id="viewport">
+				<slot></slot>
+			</div>
+			`;
+		}
+
+		/** @type {Number} backing field of attribute `zoom` */
+		_zoom
+
+		static get properties() {
+			return {
+				zoom: {
+					type: Number,
+					value: 1.0,
+					readOnly: false,
+					notify: true,
+					reflectToAttribute: true,
+					observer: '_handleZoom'
+				},
+				minZoom: {
+					type: Number,
+					value: 0.1,
+				},
+				maxZoom: {
+					type: Number,
+					value: Infinity,
+				},
+				modifierKey: {
+					type: String,
+					value: null,
+					reflectToAttribute: true,
+				}
+			}
+		}
+
+		static get observers() {
+			return []
+		}
+
+		constructor() {
+			super();
+			this._bind(this);
+		}
+
+		/**
+		 * @description bind proper `this` to viewport's handlers
+		*/
+		_bind(self) {
+			self._onWheel = self._onWheel.bind(self);
+			self._onPointerDown = self._onPointerDown.bind(self);
+			self._onPointerMove = self._onPointerMove.bind(self);
+			self._onPointerUp = self._onPointerUp.bind(self);
+		}
+
+		connectedCallback() {
+			super.connectedCallback()
+			this._attachEvents();
+			this.$.viewport.style.zoom = this.zoom
+		}
+
+		_attachEvents() {
+			this.$.viewport.addEventListener("wheel", this._onWheel);
+			this.$.viewport.addEventListener("pointerdown", this._onPointerDown);
+		}
+
+		_onWheel(e) {
+			e.preventDefault();
+			this.zoom += e.deltaY / 1000;
+		}
+
+		_onPointerDown(e) {
+			if (!this._isModifierDown(e)) return
+			e.preventDefault()
+			this.$.viewport.classList.add("manipulating")
+			this._lastPointer = [
+				e.offsetX,
+				e.offsetY
+			]
+
+			this._lastScroll = [
+				this.$.viewport.scrollLeft,
+				this.$.viewport.scrollTop
+			]
+
+			this.$.viewport.setPointerCapture(e.pointerId)
+			this.$.viewport.addEventListener("pointermove", this._onPointerMove)
+			this.$.viewport.addEventListener("pointerup", this._onPointerUp)
+		}
+		_onPointerMove(e) {
+			const currentPointer = [
+				e.offsetX,
+				e.offsetY
+			];
+			const delta = [
+				currentPointer[0] + this._lastScroll[0] - this._lastPointer[0],
+				currentPointer[1] + this._lastScroll[1] - this._lastPointer[1]
+			];
+
+			this.$.viewport.scroll(this._lastScroll[0] / this.zoom - delta[0] / this.zoom, this._lastScroll[1] / this.zoom - delta[1] / this.zoom, { behavior: "instant" });
+		}
+		_onPointerUp(e) {
+			this.$.viewport.classList.remove("manipulating");
+			this.$.viewport.removeEventListener("pointermove", this._onPointerMove)
+			this.$.viewport.removeEventListener("pointerup", this._onPointerUp)
+			this.$.viewport.releasePointerCapture(e.pointerId);
+		}
+		_isModifierDown(e) {
+			if (!this.modifierKey) return true;
+			if (this.modifierKey === "ctrl" && e.ctrlKey) return true;
+			if (this.modifierKey === "alt" && e.altKey) return true;
+			if (this.modifierKey === "shift" && e.shiftKey) return true;
+			return false;
+		}
+
+		_handleZoom(val) {
+			this._zoom = Math.min(Math.max(parseFloat(val), this.minZoom), this.maxZoom);
+			this.$.viewport.style.zoom = this._zoom;
+		}
+	}
+
+	customElements.define(ImageDiffPanBox.is, ImageDiffPanBox);
+
 	const ImageDiffToolHTML = Polymer.html`
 <style include="shared-styles">
 	:host {
@@ -35,13 +174,16 @@
 	}
 	.wrapper {
 		box-shadow: 0 1px 3px rgba(0, 0, 0, .3);
+		/*display: block;
+		overflow: auto; */
 	}
 	img {
 		display: block;
-		height: var(--img-height);
 		margin: auto;
 		position: absolute;
-		width: var(--img-width);
+		/*height: var(--img-height);
+		width: var(--img-width);*/
+		width: 100%;
 	}
 	#imageRevision {
 		opacity: var(--img-opacity);
@@ -88,12 +230,12 @@
 		<onion-diff-mode base-image="[[baseImage]]" revision-image="[[revisionImage]]"></onion-diff-mode>
 	</template>
 </div>
-<div class="wrapper">
+<image-diff-pan-box min-zoom="0.5" class="wrapper">
 	<div id="imageDiffContainer">
 		<img on-load="_onImageLoad" id="imageBase"/>
 		<img on-load="_onImageLoad" data-opacity$="{{opacityValue}}" id="imageRevision"/>
 	</div>
-</div>`;
+</image-diff-pan-box>`;
 
 	const DiffModes = {
 		ONION: 'onion',
@@ -163,24 +305,12 @@
 		static get observers() {
 			return [
 				'_handleImageChange(baseImage, revisionImage)',
-				'_handleHeightChange(_maxHeight)',
-				'_handleWidthChange(_maxWidth)',
+				// '_handleHeightChange(_maxHeight)',
+				// '_handleWidthChange(_maxWidth)',
 			];
 		}
 
 		_onImageLoad(e) {
-			this._maxHeight = Math.max(
-				this._maxHeight,
-				Polymer.dom(e).rootTarget.naturalHeight
-			);
-			this._maxWidth = Math.max(
-				this._maxWidth,
-				Polymer.dom(e).rootTarget.naturalWidth
-			);
-
-			this.updateStyles({
-				'--img-width': '100%'
-			});
 		}
 
 		_handleImageChange(baseImage, revisionImage) {
@@ -199,34 +329,6 @@
 
 		computeSrcString(image) {
 			return 'data:' + image['type'] + ';base64, ' + image['body'];
-		}
-
-		handleScaleSizesToggle() {
-			let width;
-			let height;
-			if (this.$.scaleSizesToggle.checked) {
-				width = this._maxWidth;
-				height = this._maxHeight;
-			}
-
-			this.updateStyles({
-				'--img-width': width ? width + 'px' : null,
-				'--img-height': height ? height + 'px' : null,
-			});
-		}
-
-		_handleHeightChange(height) {
-			if (!height) {
-				return;
-			}
-			this.updateStyles({ '--div-height': `${height}px` });
-		}
-
-		_handleWidthChange(width) {
-			if (!width) {
-				return;
-			}
-			this.updateStyles({ '--div-width': `${width}px` });
 		}
 	}
 
